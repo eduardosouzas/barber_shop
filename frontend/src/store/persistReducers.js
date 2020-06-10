37daa3bd3a8cb096bec41a8b256d2c70c902846f
Edu Souza <eduardomail@gmail.com>
2020-06-09 21:33:43 -0300

    changes

--- frontend/src/store/persistReducers.js
+++ frontend/src/store/persistReducers.js
@@ -1,2 +1,9 @@
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
+
+export default reducers => {
+  const persistReducer = persistReducer({
+    key: 'root',
+    
+  });
+}
